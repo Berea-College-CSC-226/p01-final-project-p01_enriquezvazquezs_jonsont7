02a446e881bb0f4f0f5b61accd007e8c1932eca0
johnsont7 <johnsont7@berea.edu>
2025-04-23 19:47:32 -0400

    Fixed Mouse_NPC.py movement

--- Maze of The Mouse/Mouse_NPC.py
+++ Maze of The Mouse/Mouse_NPC.py
@@ -1,3 +1,4 @@
+from math import hypot
 
 import pygame
 import random
@@ -14,18 +15,20 @@ class Mouse(NPC):
         Represents the Mouse NPC in the game.
         """
         super().__init__(screen_size)
+        self.move_distance = 4
 
     def movement(self, others):
         """
         Moves the Mouse away from the closest cat.
         """
-        closest_cat_pos = self.get_closest(others)
-        if closest_cat_pos:
-            direction = self.get_escape_direction(closest_cat_pos)
-            self.move_opposite(direction)
-        self.get_direction()
+        # closest_cat_pos = self.get_closest(others)
+        # if closest_cat_pos:
+        #     direction = self.get_escape_direction(closest_cat_pos)
+        #     self.move_opposite(direction)
+        # self.get_direction()
+        self.moveAwayFromOther(self.get_closest_cat_position(others))
 
-    def get_closest(self, others):
+    def get_closest_cat_position(self, others):
         """
         Finds the closest cat from a list of other sprites.
         """
@@ -49,30 +52,41 @@ class Mouse(NPC):
         b = pos1[1] - pos2[1]
         return math.sqrt(a ** 2 + b ** 2)
 
-    def get_escape_direction(self, cat_pos):
-        """
-        Determines the direction the mouse should run away.
-        """
-        dx = self.rect.centerx - cat_pos[0]
-        dy = self.rect.centery - cat_pos[1]
+    # def get_escape_direction(self, cat_pos):
+    #     """
+    #     Determines the direction the mouse should run away.
+    #     """
+    #     dx = self.rect.centerx - cat_pos[0]
+    #     dy = self.rect.centery - cat_pos[1]
+    #
+    #     if abs(dx) > abs(dy):
+    #         return "east" if dx > 0 else "west"
+    #     else:
+    #         return "south" if dy > 0 else "north"
 
-        if abs(dx) > abs(dy):
-            return "east" if dx > 0 else "west"
-        else:
-            return "south" if dy > 0 else "north"
+    # def move_opposite(self, direction):
+    #     """
+    #     Moves in the opposite direction of the closest cat,
+    #     staying within the screen boundaries.
+    #     """
+    #     distance = int(self.move_distance)
+    #
+    #     if direction == "north" and self.rect.bottom + distance <= self.screen_size[1]:
+    #         self.rect.move_ip(0, distance)  # Move down
+    #     elif direction == "south" and self.rect.top - distance >= 0:
+    #         self.rect.move_ip(0, -distance)  # Move up
+    #     elif direction == "east" and self.rect.left - distance >= 0:
+    #         self.rect.move_ip(-distance, 0)  # Move left
+    #     elif direction == "west" and self.rect.right + distance <= self.screen_size[0]:
+    #         self.rect.move_ip(distance, 0)
 
-    def move_opposite(self, direction):
-        """
-        Moves in the opposite direction of the closest cat,
-        staying within the screen boundaries.
-        """
-        distance = int(self.move_distance)
+    def moveAwayFromOther(self, other):
+        selfPos = (self.rect.centerx, self.rect.centery)
 
-        if direction == "north" and self.rect.bottom + distance <= self.screen_size[1]:
-            self.rect.move_ip(0, distance)  # Move down
-        elif direction == "south" and self.rect.top - distance >= 0:
-            self.rect.move_ip(0, -distance)  # Move up
-        elif direction == "east" and self.rect.left - distance >= 0:
-            self.rect.move_ip(-distance, 0)  # Move left
-        elif direction == "west" and self.rect.right + distance <= self.screen_size[0]:
-            self.rect.move_ip(distance, 0)
+        yDistance = selfPos[1] - other[1]
+        xDistance = selfPos[0] - other[0]
+        scale = 1/hypot(xDistance, yDistance)
+        yDistance = yDistance * scale
+        xDistance = xDistance * scale
+
+        self.rect.move_ip(xDistance*self.move_distance, yDistance*self.move_distance)
